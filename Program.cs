68457aaed1b240fdab9a75b3794d1e02fe40cc2c
Oussama <oussama.el.hallabi@student.ehb.be>
2023-10-02 15:06:16 +0200

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var wekker = new Wekker();
+
+        while (true)
+        {
+            Console.WriteLine("Wekker Menu:");
+            Console.WriteLine("1. Wekker instellen");
+            Console.WriteLine("2. Snooze instellen");
+            Console.WriteLine("3. Alarm aan/uit");
+            Console.WriteLine("4. Snooze aan/uit");
+            Console.WriteLine("5. Afsluiten");
+
+            var choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    Console.Write("Geef de tijd voor de wekker (in seconden): ");
+                    var alarmTime = TimeSpan.FromSeconds(int.Parse(Console.ReadLine()));
+                    wekker.SetAlarmTime(alarmTime);
+                    break;
+                case "2":
+                    Console.Write("Geef de tijd voor snooze (in seconden): ");
+                    var snoozeTime = TimeSpan.FromSeconds(int.Parse(Console.ReadLine()));
+                    wekker.SetSnoozeTime(snoozeTime);
+                    break;
+                case "3":
+                    wekker.TurnOnAlarm();
+                    break;
+                case "4":
+                    wekker.TurnOnSnooze();
+                    break;
+                case "5":
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("Ongeldige keuze. Probeer opnieuw.");
+                    break;
+            }
+        }
+    }
+}
---
+++ Wekker.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading;
+
+public class Wekker
+{
+    private Timer alarmTimer;
+    private Timer snoozeTimer;
+    private bool isAlarmOn = false;
+    private bool isSnoozeOn = false;
+
+    public void SetAlarmTime(TimeSpan time)
+    {
+        alarmTimer = new Timer(TriggerAlarm, null, (int)time.TotalMilliseconds, Timeout.Infinite);
+    }
+
+    public void SetSnoozeTime(TimeSpan time)
+    {
+        snoozeTimer = new Timer(TriggerSnooze, null, (int)time.TotalMilliseconds, Timeout.Infinite);
+    }
+
+    public void TurnOnAlarm()
+    {
+        isAlarmOn = true;
+    }
+
+    public void TurnOffAlarm()
+    {
+        isAlarmOn = false;
+    }
+
+    public void TurnOnSnooze()
+    {
+        isSnoozeOn = true;
+    }
+
+    public void TurnOffSnooze()
+    {
+        isSnoozeOn = false;
+    }
+
+    private void TriggerAlarm(object state)
+    {
+        if (isAlarmOn)
+        {
+            Console.WriteLine("Alarm gaat af: Geluid, Boodschap, Knipperlicht");
+            // Voer acties uit voor alarm (geluid, boodschap, knipperlicht)
+        }
+    }
+
+    private void TriggerSnooze(object state)
+    {
+        if (isSnoozeOn)
+        {
+            Console.WriteLine("Snooze gaat af: Geluid, Boodschap, Knipperlicht");
+            // Voer acties uit voor snooze (geluid, boodschap, knipperlicht)
+        }
+    }
+}
